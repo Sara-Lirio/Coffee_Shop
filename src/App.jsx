4cf0beaba305e89b5f28b88c88bc153b4e0ce4df
Sara <saraeliza.lirio@gmail.com>
2022-09-26 11:32:08 -0300

    🚧 : Adicionando Rotas no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 function App () {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <Routes>
+                <Route/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
