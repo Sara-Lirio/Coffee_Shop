5820d608cb42dea725b6295346cad4593396a968
Sara <saraeliza.lirio@gmail.com>
2022-09-26 14:30:46 -0300

    ✨ feat: Add Seção 1 da Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Menu from '../../components/Menu/Menu'
+import style from './Home.module.css'
 
 const Home = () => {
-  return (
-    <Menu/>
-  )
+    return (
+        <main>
+            <Menu />
+            <section className={style.section}>
+                <h1 className={style.sectionPhase}>O seu bom dia começa aqui!</h1>
+            </section>
+        </main>
+    )
 }
 
 export default Home
